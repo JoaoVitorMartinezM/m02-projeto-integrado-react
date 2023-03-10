5573e2128a2ceea03070b2c2e9ad31ead2f73669
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-03-09 21:52:23 -0300

    api implementada

---
+++ client/src/hooks/useCourseList/useCourseList.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react"
+import {apiService} from '../../services/api'
+
+
+
+const useCourseList = () => {
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const response = await apiService.get('/courses');
+            setError(response.error);
+            setData(response.data);
+            setIsLoading(false);
+        }
+
+        fetchData();
+    }, []);
+
+    return{
+        courses: data,
+        error,
+        isLoading,
+    }
+    
+}
+
+export default useCourseList;
