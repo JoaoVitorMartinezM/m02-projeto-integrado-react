54a100866cca04da22d3fec069d8b33e17f24014
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-03-14 22:42:06 -0300

    Adicionando userContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import Navbar from './components/Navbar';
 import PageWrapper from './components/PageWrapper';
 import Footer from './components/Footer';
+import UserContext from './contexts/UserContext';
 
 import HomePage from './pages/HomePage';
+import { useState } from 'react';
+
+
+
+
 
 function App() {
+
+  const [user, setUser] = useState({name:"Joao", isAdministrator:true});
+
   return (
     <>
-      <Navbar />
 
-      <PageWrapper>
-        <HomePage />
-      </PageWrapper>
+      <Navbar />
+      <UserContext.Provider value={[user, setUser]}>
+        <PageWrapper>
+          <HomePage />
+        </PageWrapper>
+      </UserContext.Provider>
 
       <Footer />
     </>
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,18 @@ import {Spinner} from 'phosphor-react'
 import CourseFilter from '../../components/CourseFilter';
 import CourseList from '../../components/CourseList';
 import useCourseList from '../../hooks/useCourseList';
+import { useContext } from 'react';
+import UserContext from '../../contexts/UserContext';
 
 import './HomePage.css';
 
 
 function HomePage() {
   const {courses, error, isLoading} = useCourseList();
+
+  const user = useContext(UserContext);
+  console.log(user);
+
   return (
     <div className='homePageContainer'>
       <CourseFilter />
