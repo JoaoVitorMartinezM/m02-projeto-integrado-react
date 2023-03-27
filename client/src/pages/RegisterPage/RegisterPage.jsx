bf3137be29b7aba77c8320d86937f15577e9f77b
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-03-26 21:58:39 -0300

    hook-form com yup/resolver

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,29 +1,62 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { BUTTON_VARIANT } from '../../components/Button/ButtonVariant'
 import InputGroup from '../../components/InputGroup'
 import Button from '../../components/Button'
 import './RegisterPage.css'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const schema = yup.object().shape({
+  name: yup.string().required('This is required'),
+  imageUrl: yup.string().url('A URL is required').required('This is required'),
+  category: yup.string().required('This is required'),
+  description: yup.string().required('This is required'),
+  duration: yup.string('Number is required').required('This is required'),
+  targetMarket: yup.string().required('This is required')
+})
 
 const RegisterPage = () => {
-  const [name, setName] = useState('')
-  const [urlImage, setUrlImage] = useState('')
-  const [duration, setDuration] = useState('')
-  const [category, setCategory] = useState('')
-  const [descrition, setDescrition] = useState('')
-  const [target, setTarget] = useState('')
-  const [content, setContent] = useState('')
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      name: '',
+      imageUrl: '',
+      category: '',
+      description: '',
+      duration: '',
+      targetMarket: ''
+    },
+    resolver: yupResolver(schema)
+  })
 
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
-        <div className="formContainer">
+        <form className="formContainer" onSubmit={handleSubmit(onSubmit)}>
             <h2>Register Course</h2>
-            <InputGroup labelText="Name" placeholder="Name" onChange={(event) => setName(event.target.value)}/>
-            <InputGroup labelText="Icon" placeholder="Url for icon" onChange={(event) => setUrlImage(event.target.value)}/>
-            <InputGroup labelText="Duration" placeholder="set Duration" onChange={(event) => setDuration(event.target.value)}/>
-            <InputGroup labelText="Category" placeholder="Category" onChange={(event) => setCategory(event.target.value)}/>
-            <InputGroup labelText="Description" placeholder="Type a description" onChange={(event) => setDescrition(event.target.value)}/>
-            <InputGroup labelText="Public Target" placeholder="Public Target" onChange={(event) => setTarget(event.target.value)}/>
-            <InputGroup labelText="Content" placeholder="Content" onChange={(event) => setContent(event.target.value)}/>
-            <Button>Register</Button>
-        </div>
+            <div>
+              <InputGroup labelText="Name" placeholder="Name" { ...register('name') } />
+              <InputGroup labelText="Icon" placeholder="Url for icon" { ...register('imageUrl') } />
+
+            </div>
+            <div>
+              <InputGroup labelText="Duration" placeholder="set Duration" { ...register('duration') } />
+              <InputGroup labelText="Category" placeholder="Category" { ...register('category') } />
+
+            </div>
+            <div>
+              <InputGroup labelText="Description" placeholder="Type a description" { ...register('description') } />
+              <InputGroup labelText="TargetMarket" placeholder="TargetMarket" { ...register('targetMarket') }/>
+
+            </div>
+            <div>
+              <InputGroup labelText="Content" placeholder="Content" />
+              <Button type='button' variant={ BUTTON_VARIANT.SECONDARY_OUTLINED }>Add</Button>
+            </div>
+            <Button type='submit' variant={ BUTTON_VARIANT.PRIMARY }>Register</Button>
+            <Button type='button' variant={ BUTTON_VARIANT.PRIMARY_LINK } onClick={ () => { } }>CANCEL</Button>
+        </form>
   )
 }
 
